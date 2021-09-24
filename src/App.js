1bc13d41f4ccc04bc700618d0e2f1195675c43a6
Dravin <kumardravid3@gmail.com>
2021-09-24 17:52:42 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,232 @@
-import logo from './logo.svg';
 import './App.css';
+import './Components/songsList'
+
+import React, { useState } from 'react';
+let key = false;
 
 function App() {
+  const [count, setCount] = useState(45)
+  function likeBtn() {
+    if(key) {
+      setCount(count-1);
+      key = false;
+    }
+    else{
+      setCount(count+1);
+      key = true;
+    }
+  }
+  <songsList />
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <>
+      <div className="app">
+        <div className="header">
+          <div className="image">
+            <img src="Radio.jpg" alt="" />
+            <img src="Logo.png" alt="" />
+            <img src="Sign-out.png" alt="" />
+          </div>
+
+          <div className="text">
+            <span>React Tracks</span>
+            <span className="h2">Dravin</span>
+            <span>Sign Out</span>
+          </div>
+        </div>
+
+        <div className="buttons">
+          <div className="search">
+            <button className="btn">
+              <i class="fa fa-close"></i>
+            </button>
+            <input
+              className="srch"
+              type="text"
+              placeholder="Search Songs Here"
+            />
+            <button className="btn" type="submit">
+              <i class="fa fa-search"></i>
+            </button>
+          </div>
+        </div>
+
+        <div className="list">
+          <div className="container">
+            <div className="btns">
+              <i onClick={likeBtn} class="fa fa-thumbs-up"></i>
+              <span>{count}</span>
+            </div>
+
+            <div className="text">
+              <span>Title</span>
+              <span>Subtitle</span>
+            </div>
+
+            <div className="track">
+              <i className="fas fa-play-circle" id="play" title="Play"></i>
+              <div className="duration">
+                <span>00:00</span>/<span>00:00</span>
+              </div>
+              <div className="slideContainer">
+                <input
+                  type="range"
+                  min="1"
+                  max="100"
+                  value="50"
+                  className="slider"
+                  id="myRange"
+                ></input>
+              </div>
+              <img src="Sound.png" alt="" />
+              <img src="Dot.jpg" alt="" />
+            </div>
+
+            <button title="Collapse" type="button" class="collapse">
+              v
+            </button>
+          </div>
+          <div className="container">
+            <div className="btns">
+              <i onClick={likeBtn} class="fa fa-thumbs-up"></i>
+              <span>{count}</span>
+            </div>
+
+            <div className="text">
+              <span>Title</span>
+              <span>Subtitle</span>
+            </div>
+
+            <div className="track">
+              <i className="fas fa-play-circle" id="play" title="Play"></i>
+              <div className="duration">
+                <span>00:00</span>/<span>00:00</span>
+              </div>
+              <div className="slideContainer">
+                <input
+                  type="range"
+                  min="1"
+                  max="100"
+                  value="50"
+                  className="slider"
+                  id="myRange"
+                ></input>
+              </div>
+              <img src="Sound.png" alt="" />
+              <img src="Dot.jpg" alt="" />
+            </div>
+
+            <button title="Collapse" type="button" class="collapse">
+              v
+            </button>
+          </div>
+          <div className="container">
+            <div className="btns">
+              <i onClick={likeBtn} class="fa fa-thumbs-up"></i>
+              <span>{count}</span>
+            </div>
+
+            <div className="text">
+              <span>Title</span>
+              <span>Subtitle</span>
+            </div>
+
+            <div className="track">
+              <i className="fas fa-play-circle" id="play" title="Play"></i>
+              <div className="duration">
+                <span>00:00</span>/<span>00:00</span>
+              </div>
+              <div className="slideContainer">
+                <input
+                  type="range"
+                  min="1"
+                  max="100"
+                  value="50"
+                  className="slider"
+                  id="myRange"
+                ></input>
+              </div>
+              <img src="Sound.png" alt="" />
+              <img src="Dot.jpg" alt="" />
+            </div>
+
+            <button title="Collapse" type="button" class="collapse">
+              v
+            </button>
+          </div>
+          <div className="container">
+            <div className="btns">
+              <i onClick={likeBtn} class="fa fa-thumbs-up"></i>
+              <span>{count}</span>
+            </div>
+
+            <div className="text">
+              <span>Title</span>
+              <span>Subtitle</span>
+            </div>
+
+            <div className="track">
+              <i className="fas fa-play-circle" id="play" title="Play"></i>
+              <div className="duration">
+                <span>00:00</span>/<span>00:00</span>
+              </div>
+              <div className="slideContainer">
+                <input
+                  type="range"
+                  min="1"
+                  max="100"
+                  value="50"
+                  className="slider"
+                  id="myRange"
+                ></input>
+              </div>
+              <img src="Sound.png" alt="" />
+              <img src="Dot.jpg" alt="" />
+            </div>
+
+            <button title="Collapse" type="button" class="collapse">
+              v
+            </button>
+          </div>
+          <div className="container">
+            <div className="btns">
+              <i onClick={likeBtn} class="fa fa-thumbs-up"></i>
+              <span>{count}</span>
+            </div>
+
+            <div className="text">
+              <span>Title</span>
+              <span>Subtitle</span>
+            </div>
+
+            <div className="track">
+              <i className="fas fa-play-circle" id="play" title="Play"></i>
+              <div className="duration">
+                <span>00:00</span>/<span>00:00</span>
+              </div>
+              <div className="slideContainer">
+                <input
+                  type="range"
+                  min="1"
+                  max="100"
+                  value="50"
+                  className="slider"
+                  id="myRange"
+                ></input>
+              </div>
+              <img src="Sound.png" alt="" />
+              <img src="Dot.jpg" alt="" />
+            </div>
+
+            <button title="Collapse" type="button" class="collapse">
+              v
+            </button>
+          </div>
+        </div>
+        <a href="/">
+          <img src="Add.png" alt="" />
         </a>
-      </header>
-    </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/Components/songsList.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function songsList() {
+    const songs = [
+      {
+        name: "Chale_Aana",
+        title: "Juda Hm Ho Gye Jana",
+        artist: "Armaan Malik",
+      },
+      {
+        name: "Kaun_Tujhe",
+        title: "Kon Tujhe Yu Payar Krega",
+        artist: "Palak Muchhal",
+      },
+    ];
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default songsList
